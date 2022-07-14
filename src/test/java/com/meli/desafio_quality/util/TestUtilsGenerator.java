a40ddf246a633dfc5a2274431632d430bd565509
Matheus Bazzo <matheus.bazzo@mercadolivre.com>
2022-07-14 11:58:10 -0300

    Test unit controller,service getAreaRoom

--- src/test/java/com/meli/desafio_quality/util/TestUtilsGenerator.java
+++ src/test/java/com/meli/desafio_quality/util/TestUtilsGenerator.java
@@ -3,6 +3,7 @@ package com.meli.desafio_quality.util;
 import com.meli.desafio_quality.model.District;
 import com.meli.desafio_quality.model.Property;
 import com.meli.desafio_quality.model.Room;
+import com.meli.desafio_quality.model.RoomDto;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +15,28 @@ public class TestUtilsGenerator {
     }
 
     public static Property getNewProperty() {
+        return Property.builder()
+                .name("Casa teste 1")
+                .district(District.builder()
+                        .name("Bairro teste 1")
+                        .valueM2(500.0)
+                        .build()
+                )
+                .listRoom(Arrays.asList(
+                        Room.builder()
+                                .name("Quarto teste 1")
+                                .roomLength(9.00)
+                                .roomWidth(6.00)
+                                .build(),
+                        Room.builder()
+                                .name("Quarto teste 2")
+                                .roomLength(8.00)
+                                .roomWidth(7.00)
+                                .build())
+                ).build();
+    }
+
+    public static Property getPropertyWithId() {
         return Property.builder()
                 .id(1L)
                 .name("Casa teste 1")
@@ -34,6 +57,32 @@ public class TestUtilsGenerator {
                                 .roomWidth(7.00)
                                 .build())
                 ).build();
+    }
+
+    public static List<Room> getRoom() {
+        return Arrays.asList(
+                Room.builder()
+                        .name("Quarto teste 1")
+                        .roomLength(9.00)
+                        .roomWidth(6.00)
+                        .build(),
+                Room.builder()
+                        .name("Quarto teste 2")
+                        .roomLength(8.00)
+                        .roomWidth(7.00)
+                        .build());
+    }
+
+    public static List<RoomDto> getRoomDto() {
+        return Arrays.asList(
+                RoomDto.builder()
+                        .name("Quarto teste 1")
+                        .area(54.0)
+                        .build(),
+                RoomDto.builder()
+                        .name("Quarto teste 2")
+                        .area(56.0)
+                        .build());
     }
 
 }
