842778d6aaeaa008f631c3731d02c8c86be46e5e
gucotrim <gustavo.cotrim@mercadolivre.com>
2022-07-15 11:22:17 -0300

    Added validation exception

--- src/main/java/com/meli/desafio_quality/handler/NotFoundExeHandler.java
+++ src/main/java/com/meli/desafio_quality/handler/NotFoundExeHandler.java
@@ -3,18 +3,26 @@ package com.meli.desafio_quality.handler;
 import com.meli.desafio_quality.exception.ExceededStock;
 import com.meli.desafio_quality.exception.ExceptionDetails;
 import com.meli.desafio_quality.exception.NotFoundException;
+import com.meli.desafio_quality.exception.ValidationExceptionDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class NotFoundExeHandler {
+public class NotFoundExeHandler extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler({NotFoundException.class})
@@ -57,4 +65,22 @@ public class NotFoundExeHandler {
         );
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<FieldError> errors = ex.getBindingResult().getFieldErrors();
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .title("Campos inválidos")
+                        .message(ex.getClass().getName())
+                        .fields(errors.stream().map(
+                                FieldError::getField).collect(Collectors.joining("; ")))
+                        .fieldsMessages(errors.stream().map(
+                                FieldError::getDefaultMessage).collect(Collectors.joining("; ")))
+                        .build()
+                , status
+        );
+    }
 }
